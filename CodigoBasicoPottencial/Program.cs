bb0c2ab95e0e03a76474dfaedd47ae3e3c70e6a1
Davez99 <davi.mdr@gmail.com>
2023-01-16 19:40:23 -0300

    Subtraindo o produto e a soma de um número inteiro

--- CodigoBasicoPottencial/Program.cs
+++ CodigoBasicoPottencial/Program.cs
@@ -1,12 +1,14 @@
-﻿// int n = int.Parse(Console.ReadLine());
+﻿//Subtraindo o produto e a soma de um número inteiro -DEV Davez
+
+// int n = int.Parse(Console.ReadLine());
 int n = int.Parse("234");
 int p = 1, s = 0;
 while (n > 0){
 int l = n % 10;
 // TODO: Crie as outras condições necessárias para a resolução do desafio:
 p *= l;
-// s +=      ;
-// n /=      ;
+s +=  l;
+n /=  10;
 } 
-
+Console.WriteLine($"teste de valores, valor de p {p} e valor de s {s}");
 Console.WriteLine(p-s);
