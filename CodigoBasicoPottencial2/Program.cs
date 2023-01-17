d91af37441d432803648e826f91f93adab43890a
Davez99 <davi.mdr@gmail.com>
2023-01-16 21:51:44 -0300

    CodigoBasicoPottencial2

--- CodigoBasicoPottencial2/Program.cs
+++ CodigoBasicoPottencial2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Encaixa ou Não? - DEV Davez
+int qt = int.Parse("4 56234523485723854755454545478690 78690 5434554 543 1243 1243 54 64545454545454545454545454545454554");
+Console.WriteLine($"teste quantidade {qt}");
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        // for (                 )
+        // {
+            
+        //     string a = v[0];
+        //     string b = v[1];
+
+        //     if (                  )
+        //         Console.WriteLine("nao encaixa");
+        //     else if (                                )
+        //         Console.WriteLine("encaixa");
+        //     else
+        //         Console.WriteLine("nao encaixa");
+
+        // }
