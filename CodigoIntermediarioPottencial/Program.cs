16b11ca7d3d560f9705f737aebe6d10240ce1fa3
Davez99 <davi.mdr@gmail.com>
2023-01-19 11:21:50 -0300

    Elemento Majoritário (Moda)

---
+++ CodigoIntermediarioPottencial/Program.cs
@@ -0,0 +1,69 @@
+﻿//1 / 3 - Elemento Majoritário - DEV Davez
+using System;
+using System.Text.RegularExpressions;
+
+/*
+Esse trecho pertence ao metodo Main, principal
+*/
+        //declaração teste;
+        //int n = int.Parse("5");
+
+        //declaração da atividade;
+        int n = int.Parse(Console.ReadLine());
+        
+        int[] num = new int[n];
+    
+        for (int i = 0; i < num.Length ; i++)
+        {
+            //declaração teste;
+            // int x, y;
+            // x = 7;
+            // y = 5;
+            
+
+            // if(i == 0){
+            //     num[i] = x;
+            // }else if (i == 1){
+            //     num[i] = x;
+            // }else if (i == 2){
+            //     num[i] = x;
+            // }else if (i == 3){
+            //     num[i] = y;
+            // }else{
+            //     num[i] = y;
+            // }
+            
+
+            //declaração da atividade;
+            num[i] = int.Parse(Console.ReadLine());
+            
+        }
+        Console.WriteLine(MajorityElement(num));
+
+    static int MajorityElement(int[] nums)
+    {
+        int major = 0;
+        int referencia = nums[0];
+        int count = 1;
+        int maxCount = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (referencia != nums[i])
+            {
+                referencia = nums[i];
+            }
+            for(int j = 0;j < nums.Length; j++){
+
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                    if(count > maxCount){
+                        major = nums[i]; 
+                        maxCount++;
+                    }
+                }               
+            } 
+            count = 1;           
+        }
+        return  major;
+    }
